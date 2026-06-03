01d2c10cd963c6363d7b646907edc3c3f6cf33f7
Sebastijan Zindl <sebastijan.zindl@proton.ch>
2026-06-03 10:35:20 +0200

    chore: update TestTLSSignedCertTrustedPublicKey SSL pin

--- internal/dialer/dialer_pinning_test.go
+++ internal/dialer/dialer_pinning_test.go
@@ -105,7 +105,7 @@ func TestTLSSignedCertTrustedPublicKey(t *testing.T) {
 
 	_, dialer, _, checker, _ := createClientWithPinningDialer("")
 	copyTrustedPins(checker)
-	checker.trustedPins = append(checker.trustedPins, `pin-sha256="r7+Y9IrFRzWd5QmvbBYd3ht+/3NzuIebSWXNmyNvAQo="`)
+	checker.trustedPins = append(checker.trustedPins, `pin-sha256="iLgal2OgR1YjHfZwGl0RHMBlYkvca3iCi2HLPTGVRxg="`)
 	_, err := dialer.DialTLSContext(context.Background(), "tcp", "rsa4096.badssl.com:443")
 	r.NoError(t, err, "expected dial to succeed because public key is known and cert is signed by CA")
 }
